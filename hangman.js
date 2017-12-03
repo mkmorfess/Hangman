9c5af28c06b05550d95c1a5d87fb965c4a4ceb1f
Michael Morfessis <mkmorfess@yahoo.com>
2017-12-02 23:18:49 -0600

    Updated with some interaction with user guess and display on prompt

--- hangman.js
+++ hangman.js
@@ -63,7 +63,8 @@ function chooseCategory () {
 
 					if (ans.correct === "Yes") {
 						var random = Math.floor(Math.random() * categories.animals.length)
-						randomWord = categories.animals[random];
+						randomWord = "giraffe";
+						// categories.animals[random];
 						console.log("Great! Here's your first word.")
 						hangman();
 					}
@@ -124,12 +125,11 @@ function chooseCategory () {
 
 
 function hangman() {
-	guesses = 9
-	var len = randomWord.length;
+	guesses = 9;
 	console.log(randomWord);
 	
 
-	for (var i = 1; i < len; i++) {
+	for (var i = 1; i < randomWord.length; i++) {
 		if (randomWord[i] === " ") {
 			displayWord += " "
 		}
@@ -141,48 +141,75 @@ function hangman() {
 }
 
 function hangmanStart(){
+	var randomWordLetters = [];
+
+	for (var i = 0; i < randomWord.length; i++) {
+		randomWordLetters.push(randomWord.charAt(i));
+	}
+
+	// console.log(randomWordLetters[4]);
 
 	inquirer.prompt([{
 		name: "letter",
 		message: displayWord + "\nGuess a letter: \n"
 	}]).then(function(ans){
-		console.log("You chose " + ans.letter + "\n");
 
-	if (guesses > 1) {	
+			console.log("You chose " + ans.letter + "\n");
 
-		if (ans.letter === randomWord.indexOf(ans.letter)) {
-			console.log("this matches");
-		}
 
-		else {
-			guesses--
-			console.log("Letters do not match");
-			console.log("Guesses Left: " + guesses);
-			hangmanStart();
-		}
-	}
 
-	else {
-		console.log("Game Over");
-		guesses = 9
-		
-		inquirer.prompt([
-			{
-				name: "newGame",
-				message: "Do you want to play a new game?",
-				type: "list",
-				choices: ["Yes", "No"]
-			}
-		]).then(function(ans){
-			if (ans.newGame === "Yes") {
-				chooseCategory();
-			}
-			else {
-				console.log("Ok.. come back real soon!");
-			}
-		})
+				if (guesses > 1 && randomWord != displayWord) {	
 
-	}
+					
+
+					if (randomWordLetters.includes(ans.letter) === true) {
+
+						for (var i = 0; i < displayWord.length; i++) {
+							if (randomWordLetters[i] === ans.letter) {
+								
+									displayWord = displayWord.replace(displayWord[i] + displayWord[i + 1], randomWordLetters[i]);
+								
+							}
+							
+						}
+
+						if (randomWord === displayWord) {
+							console.log("You win!")
+						}
+						else {	
+						hangmanStart();
+						}
+					}
+					else {
+						guesses--
+						console.log("Letters do not match");
+						console.log("Guesses Left: " + guesses);
+						hangmanStart();
+					}
+				}
+
+
+				else {
+					console.log("Game Over");
+					guesses = 9
+					
+					inquirer.prompt([
+						{
+							name: "newGame",
+							message: "Do you want to play a new game?",
+							type: "list",
+							choices: ["Yes", "No"]
+						}
+					]).then(function(ans){
+						if (ans.newGame === "Yes") {
+							chooseCategory();
+						}
+						else {
+							console.log("Ok.. come back real soon!");
+						}
+					})
+
+				}
 
 	})
 
